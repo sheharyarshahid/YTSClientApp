26d6509af98b1676034d9746788782ea6553f8b6
Sheharyar Shahid <sshary019@gmail.com>
2020-12-06 02:03:10 +0500

    Add project files.

---
+++ MoviesAp/MoviesAp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MoviesAp.Services;
+using MoviesAp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoviesAp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MoviesAp/MoviesAp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using MoviesAp.ViewModels;
+using Xamarin.Forms;
+
+namespace MoviesAp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
